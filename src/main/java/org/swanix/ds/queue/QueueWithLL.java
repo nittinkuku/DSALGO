8777256bf88f5668f8580e8fd414c80077b0fecc
nitink <nittinkuku@gmail.com>
2020-05-24 23:58:28 +0530

    Queue With Array

--- src/main/java/org/swanix/ds/queue/QueueWithLL.java
+++ src/main/java/org/swanix/ds/queue/QueueWithLL.java
@@ -1,8 +1,8 @@
 package org.swanix.ds.queue;
 
-public class Queue {
-    private Node first;
-    private Node last;
+public class QueueWithLL {
+    private Node head;
+    private Node tail;
 
     private class Node {
         private int data;
@@ -13,22 +13,22 @@ public class Queue {
         Node node = new Node();
         node.data = data;
         node.next = null;
-        if (last == null) {
-            last = first = node;
+        if (isEmpty()) {
+            tail = head = node;
         } else {
-            last.next = node;
-            last = node;
+            tail.next = node;
+            tail = node;
         }
         System.out.println(data + " is Stored in Queue");
     }
 
     public int dequeue() {
         if (!isEmpty()) {
-            int data = first.data;
-            first = first.next;
+            int data = head.data;
+            head = head.next;
             System.out.println(data + " retrieved from Queue");
-            if (first == null) {
-                first = last = null;
+            if (head == null) {
+                head = tail = null;
             }
             return data;
         } else {
@@ -38,7 +38,7 @@ public class Queue {
     }
 
     public boolean isEmpty() {
-        if (first == null) {
+        if (head == null) {
             return true;
         } else {
             return false;
@@ -51,7 +51,7 @@ public class Queue {
             return 0;
         } else {
             int size = 0;
-            Node tempNode = first;
+            Node tempNode = head;
             while (tempNode != null) {
                 size++;
                 tempNode = tempNode.next;
@@ -62,20 +62,20 @@ public class Queue {
     }
 
     public static void main(String[] args) {
-        Queue queue = new Queue();
-        queue.dequeue();
-        queue.dequeue();
-        queue.size();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.size();
-        queue.enqueue(3);
-        queue.size();
-        queue.dequeue();
-        queue.dequeue();
-        queue.size();
-        queue.dequeue();
-        queue.size();
-        queue.dequeue();
+        QueueWithLL queueWithLL = new QueueWithLL();
+        queueWithLL.dequeue();
+        queueWithLL.dequeue();
+        queueWithLL.size();
+        queueWithLL.enqueue(1);
+        queueWithLL.enqueue(2);
+        queueWithLL.size();
+        queueWithLL.enqueue(3);
+        queueWithLL.size();
+        queueWithLL.dequeue();
+        queueWithLL.dequeue();
+        queueWithLL.size();
+        queueWithLL.dequeue();
+        queueWithLL.size();
+        queueWithLL.dequeue();
     }
 }
