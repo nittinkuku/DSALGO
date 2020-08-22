42969f25e3fa18bd2559ce7b6c43464e4871bac6
nitink <nittinkuku@gmail.com>
2020-08-23 00:26:26 +0530

    graph - connected component

---
+++ src/main/java/org/swanix/dsalgo/graph/ConnectedComponent.java
@@ -0,0 +1,36 @@
+package org.swanix.dsalgo.graph;
+
+public class ConnectedComponent {
+    private boolean[] marked;
+    private int[] id;
+    private int count;
+
+    public ConnectedComponent(Graph graph) {
+        marked = new boolean[graph.vertices()];
+        id = new int[graph.vertices()];
+        for (int v = 0; v < graph.vertices(); v++) {
+            if (!marked[v]) {
+                dfs(graph, v);
+                count++;
+            }
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public int id(int v) {
+        return id[v];
+    }
+
+    private void dfs(Graph graph, int v) {
+        marked[v] = true;
+        id[v] = count;
+        for (int w : graph.adjacent(v)) {
+            if (!marked[w]) {
+                dfs(graph, w);
+            }
+        }
+    }
+}
