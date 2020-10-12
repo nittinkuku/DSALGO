1436f2dd695deaa3eb7d56a51b6bcbd977e20bdf
nitink <nittinkuku@gmail.com>
2020-10-13 04:23:12 +0530

    LeetCode

---
+++ src/main/java/org/swanix/leetcode/medium/GenerateParentheses.java
@@ -0,0 +1,44 @@
+package org.swanix.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, String cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max)
+            backtrack(ans, cur+"(", open+1, close, max);
+        if (close < open)
+            backtrack(ans, cur+")", open, close+1, max);
+    }
+
+}
+
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+
+
+Constraints:
+
+1 <= n <= 8
+* */
